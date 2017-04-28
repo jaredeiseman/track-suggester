620b885cc5e6bc38a075739f82b9f839903f33df
Jared Eiseman <jathei@gmail.com>
2017-04-28 09:45:22 -0700

    adjust comments in scripts.js to reflect plan for business logic, add click handlers to next and previous buttons for form navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
+////////////////////////////////////////////////////////////////////////////////
+//                              BUSINESS LOGIC                                //
+////////////////////////////////////////////////////////////////////////////////
+//determination logic
+
+////////////////////////////////////////////////////////////////////////////////
+//                           USER INTERFACE LOGIC                             //
+////////////////////////////////////////////////////////////////////////////////
 $(document).ready(function() {
   //next button to hide current '.question-content' div and show next
+  $('button[name="next"]').click(function() {
+    $(this).parent().slideUp();
+    $(this).parent().next().slideDown();
+  });
 
   //previous button to hide current '.question-content' div and show previous
+  $('button[name="previous"]').click(function() {
+    $(this).parent().slideUp();
+    $(this).parent().prev().slideDown();
+  });
 
   //submit button handler
     //block submission
@@ -13,9 +29,10 @@ $(document).ready(function() {
       //create error at top of page saying to fill out all questions
       //remove has-error/messaging from each question filled out after submit is clicked again
 
-    //logic for determining track (business logic?)
+    //call business logic
 
     //display results
       //hide form
       //show appropriate track w/ description
+  });
 });
