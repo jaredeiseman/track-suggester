5909aa55dea257ba3adfab917b0ab6eacc33fa18
Jared Eiseman <jathei@gmail.com>
2017-04-28 09:33:18 -0700

    hide questions to form past the first one in styles.css, plan out javascript with comments in scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  //next button to hide current '.question-content' div and show next
+
+  //previous button to hide current '.question-content' div and show previous
+
+  //submit button handler
+    //block submission
+
+    //gather form values
+
+    //form validate (bind?)
+      //add has-error and messaging to divs missing a value
+      //create error at top of page saying to fill out all questions
+      //remove has-error/messaging from each question filled out after submit is clicked again
+
+    //logic for determining track (business logic?)
+
+    //display results
+      //hide form
+      //show appropriate track w/ description
+});
